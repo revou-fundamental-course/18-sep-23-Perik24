68e8952ec51fc3dd9651a100dd209b35bb2109a8
Verick Lesly <verick223@gmail.com>
2023-09-27 20:12:06 +0700

    validation

--- js/script.js
+++ js/script.js
@@ -13,7 +13,22 @@ let slideIndex = 0;
       slides[slideIndex - 1].style.display = "block";
       setTimeout(showSlides, 3000); 
     }
-
-    
     showSlides();
+
+    function validateForm() {
+      const nameInput = document.getElementById("name").value;
+      const emailInput = document.getElementById("email").value;
+
+      if (nameInput.trim() === "") {
+          alert("Please enter your name.");
+      } else if (emailInput.trim() === "") {
+          alert("Please enter your email address.");
+      } else {
+          // Form is valid, you can submit it here
+          alert("Form is valid. Submitting...");
+      }
+  }
+
+
+
 
